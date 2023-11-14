d5c2fbacc1d91aec345059646f03dc705fc547e6
mvuk0j4 <vukojamateo7@gmail.com>
2023-11-14 12:21:48 +0100

    new commit

--- app/src/main/java/ba/sum/fsre/mojanovaaplikacija/ProfileActivity.java
+++ app/src/main/java/ba/sum/fsre/mojanovaaplikacija/ProfileActivity.java
@@ -1,13 +1,23 @@
 package ba.sum.fsre.mojanovaaplikacija;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.internal.EdgeToEdgeUtils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import ba.sum.fsre.mojanovaaplikacija.model.User;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -28,6 +38,45 @@ public class ProfileActivity extends AppCompatActivity {
 
         this.loggedUser = this.auth.getCurrentUser();
 
-        DatabaseReference usersDatabaseRef  = this.db.getReference("users");
+        DatabaseReference usersDatabaseRef  = this.db.getReference();
+
+        EditText firstnameTxt = findViewById(R.id.nameTxt);
+        EditText lastnameTxt = findViewById(R.id.surnameTxt);
+        EditText phoneTxt = findViewById(R.id.phoneTxt);
+        EditText addressTxt = findViewById(R.id.phoneTxt);
+        EditText placeTxt = findViewById(R.id.placeTxt);
+        EditText countryTxt = findViewById(R.id.countryTxt);
+
+        Button saveBtn = findViewById(R.id.saveBtn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User currentUser = new User(
+                        firstnameTxt.getText().toString(),
+                        lastnameTxt.getText().toString(),
+                        phoneTxt.getText().toString(),
+                        addressTxt.getText().toString(),
+                        placeTxt.getText().toString(),
+                        countryTxt.getText().toString()
+
+                );
+
+                usersDatabaseRef.child(loggedUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            User currentUser = task.getResult().getValue(User.class);
+                            firstnameTxt.setText(currentUser.firstname);
+                            lastnameTxt.setText(currentUser.lastname);
+                            phoneTxt.setText(currentUser.phone);
+                            addressTxt.setText(currentUser.address);
+                            placeTxt.setText(currentUser.place);
+                            countryTxt.setText(currentUser.country);
+                        }
+                    }
+                });
+            }
+        });
     }
 }
